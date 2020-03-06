f745e5b5c95e344d709d11fb156c6d3127c6e1e1
Martin Morris <martin.morris95@hotmail.com>
2020-03-06 16:50:41 +1000

    new

--- email.php
+++ email.php
@@ -1,41 +1,60 @@
+
+
 <?php
- 
+require 'PHPMailer-master/PHPMailerAutoload.php';
+
 if($_POST) {
-    $visitor_name = "";
-    $visitor_email = "";
-    $visitor_message = "";
-     
-    if(isset($_POST['visitor_name'])) {
-        $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    }
-     
-    if(isset($_POST['email'])) {
-        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
-    }
-     
-     
-    if(isset($_POST['text'])) {
-        $visitor_message = htmlspecialchars($_POST['text']);
-    }
-     
-   
-    $recipient = "martin.morris@thomsonadsett.com";
-    
    
      
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . '$visitor_email' . "\r\n";
-     
-    if(mail($recipient, "Website Query", $visitor_message, $headers)) {
-        echo "<p>Thank you for contacting us, $visitor_name. We will be in touch soon.</p>";
-    } else {
-        echo '<p>We are sorry but the email did not go through.</p>';
-    }
-     
+    $email='martin.morris@outlook.com';//change email to receipents email
+
+$visitor_name = "";
+$visitor_email = "";
+$visitor_message = "";
+
+if(isset($_POST['visitor_name'])) {
+    $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+}
+ 
+if(isset($_POST['email'])) {
+    $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+    $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+}
+ 
+ 
+if(isset($_POST['text'])) {
+    $visitor_message = htmlspecialchars($_POST['text']);
+}
+ 
+
+$mail = new PHPMailer();
+$mail->SMTPDebug = 0; 
+$mail->SMTPAuth = false;//SMTP authentication should be false
+$mail->SMTPSecure = 'none';// Security type should be none 
+
+$mail->Host = 'localhost';// SMTP host name should be localhost
+$mail->Port = 25;  
+
+$mail->setFrom($visitor_email, 'test email');     //Set who the message is to be sent from
+
+//$mail->addReplyTo('test@yourdomain.com', 'test email');  //Set an alternative reply-to address
+$mail->addAddress($email);  // Add a recipient
+$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = $visitor_email;
+
+$mail->Body    = $visitor_message;
+if(!$mail->send()) {
+   echo 'Mailer Error: ' . $mail->ErrorInfo;
+   echo 'Email Has not Sent';
+   exit;
+}
+else{
+  echo "email sent";
+}
+
 } else {
     echo '<p>Something went wrong</p>';
 }
- 
+
 ?>
